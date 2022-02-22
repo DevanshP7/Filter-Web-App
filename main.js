ca629acfe628eca4a9b283d0d57fdd0511846710
DevanshP7 <79050720+DevanshP7@users.noreply.github.com>
2022-02-22 20:22:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-function preload(){}
+nose_x=0;
+nose_y=0;
+picture='hat';
+function preload(){
+    image_png=loadImage('https://i.postimg.cc/cHz86ywX/mustache.png');
+    image_hat=loadImage('https://i.postimg.cc/0NWL6cQV/Male-Transparent-Hat-PNG-Clipart.png');
+}
 function setup(){
     canvas=createCanvas(640,480);
-    canvas.center();
+    canvas.position(650,350);
     video=createCapture(VIDEO);
     video.size(640,480);
     video.hide();
@@ -10,6 +16,13 @@ function setup(){
 }
 function draw(){
     image(video,0,0,640,480);
+    if(picture=='mustache'){image(image_png,nose_x-50,nose_y+5,100,30);}
+    if(picture=='hat'){image(image_hat,nose_x-150,nose_y-200,300,150);}
+    if(picture=='both'){
+        image(image_png,nose_x-50,nose_y+5,100,30);
+        image(image_hat,nose_x-150,nose_y-200,300,150);
+    }
+    
 }
 function take_snapshot(){
     save('Devansh.png');
@@ -19,8 +32,10 @@ function posenet_loaded(){
 }
 function got_pose(results){
     if(results.length>0){
-        console.log(results);
-        console.log('X position of nose = '+results[0].pose.nose.x);
-        console.log('Y position of nose = '+results[0].pose.nose.y);
+        nose_x=results[0].pose.nose.x;
+        nose_y=results[0].pose.nose.y;
     }
 }
+function mustache_only(){picture='mustache';}
+function hat_only(){picture='hat';}
+function both(){picture='both';}
